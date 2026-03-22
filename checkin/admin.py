bf1cb10149db9a6116caad0462cd291b12ad5153
Kathe <katta.vergara.libertad@gmail.com>
2026-03-22 14:06:44 -0300

    Admin configurado

--- checkin/admin.py
+++ checkin/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Paciente, Doctor, Cita, RegistroVisita
 
-# Register your models here.
+admin.site.register(Paciente)
+admin.site.register(Doctor)
+admin.site.register(Cita)
+admin.site.register(RegistroVisita)
